14f55a548c9156fd0463352f3aa63f3f15c4f4c4
Monike Freitas <monike.ftsousa@gmail.com>
2021-07-12 15:00:07 -0400

    CSS

--- index.js
+++ index.js
@@ -1,53 +1,79 @@
-var fila = []
-var atual = 1
+var fila = [];
+var atual = 1;
 
-$(document).ready(function(){
-    for(var i=1;i<=4;i++){
-        $('.andares').append('<div><i class="cima material-icons">arrow_upward</i> <i class="baixo material-icons">arrow_downward</i><div class="porta"></div><div class="porta"></div></div>');
-        $('.buttons div:eq(' + (i-1) + ')').click(function(){
-            if(fila.length<3)
-                add($(this).text()-0)
-        })
-    }
-})
+$(document).ready(function () {
+  for (var i = 1; i <= 4; i++) {
+    $(".andares").append(
+      '<div><div class="porta"><div class="painel"></div><div class="portas"><div class="p1"></div><div class="p2"></div></div><div class="botoes"><i class="cima material-icons">arrow_upward</i><i class="baixo material-icons">arrow_downward</i></div></div></div>'
+    );
+    $(".buttons div:eq(" + (i - 1) + ")").click(function () {
+      if (fila.length < 3) add($(this).text() - 0);
+    });
+  }
+});
 
-function add(andar){
-    if(atual!=andar){
-        fila.push(andar)
-        $('.buttons div:eq('+(andar-1)+')').css('background-color', 'green');
-        wait()
-
-    }
+function add(andar) {
+  if (atual != andar) {
+    fila.push(andar);
+    $(".buttons div:eq(" + (andar - 1) + ")").css("background-color", "green");
+    wait();
+  }
 }
 
 //funcao recursiva
-function wait (){
-    var aux = atual - fila[0]
-    aux = aux<0?-aux:aux
+function wait() {
+  var aux = atual - fila[0];
+  aux = aux < 0 ? -aux : aux;
 
-    $('.tunel .box').animate({top: ((3 -(fila[0]-1)) * 100)+ 'px'}, aux*1000,  function(){
-        atual = fila[0]?fila[0]:atual;
-        $('.display h2').text(atual)
-        $('.buttons div:eq('+(atual-1)+')').css('background-color', 'rgb(133, 132, 132)');
+  $(".tunel .box").animate(
+    { top: (3 - (fila[0] - 1)) * 300 + "px" },
+    aux * 1000,
+    function () {
+      atual = fila[0];
+      console.log("atual:", atual, " fila 0: ", fila[0]);
+      abrirPortas(4 - fila[0]);
 
-        espera(3);
-    });
+      $(".display h2").text(atual);
+      $(".buttons div:eq(" + (atual - 1) + ")").css(
+        "background-color",
+        "rgb(133, 132, 132)"
+      );
 
-}
-
-function espera (i){
-    if(i==0) {
-        fila.shift();
-
-        $('.tunel .box').css('top','300px')
-        return;
+      espera(3);
     }
-    $('.tunel .box').animate({top: '+=0px'}, 1000, function (){
-        if(fila.length>0){
-            fila.shift();
-            wait()
-        } else {
-            return espera(i-1)
-        }
-    })
+  );
 }
+
+function espera(i) {
+  if (i == 0) {
+    fecharPortas(4 - atual);
+    fila.shift();
+    atual = 1;
+
+    $(".display h2").text(atual);
+    $(".tunel .box").animate({ top: "900px" }, 2000);
+    return;
+  }
+  $(".tunel .box").animate({ top: "+=0px" }, 1000, function () {
+    if (fila.length > 1) {
+      fecharPortas(4 - atual);
+      fila.shift();
+      wait();
+    } else {
+      return espera(i - 1);
+    }
+  });
+}
+
+function abrirPortas(andar) {
+  $(".porta:eq(" + andar + ") .portas .p2").animate({ right: "-40px" });
+  $(".porta:eq(" + andar + ") .portas .p1").animate({ left: "-40px" });
+}
+
+function fecharPortas(andar) {
+  $(".porta:eq(" + andar + ") .portas .p2").animate({ right: "0px" });
+  $(".porta:eq(" + andar + ") .portas .p1").animate({ left: "0px" });
+}
+
+
+
